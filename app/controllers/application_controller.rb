5206ded9c695cb1ba240747f14979929fe390d1b
Kantakun018 <kan.xxi.18@gmail.com>
2024-04-07 20:00:16 +0900

    新規ユーザー登録機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
 
+  # 追記
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
 
   private
 
@@ -9,4 +12,10 @@ class ApplicationController < ActionController::Base
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"] 
     end
   end
+
+  # 追記
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name , :last_name_yomi, :first_name_yomi, :birthday])
+  end
+
 end
---
+++ app/models/user.rb
@@ -0,0 +1,28 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_yomi, presence: true
+  validates :first_name_yomi, presence: true
+  validates :birthday, presence: true
+
+  # パスワードは、半角英数字混合での入力が必須であること
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+
+  # お名前(全角)は、全角（漢字・ひらがな・カタカナ）での入力が必須であること。
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'には全角文字を使用してください' } do
+    validates :last_name
+    validates :first_name
+  end
+
+  # お名前カナ(全角)は、全角（カタカナ）での入力が必須であること。
+  validates :last_name_yomi, format: { with: /\A[ァ-ヶー－]+\z/, message: "には全角カタカナを使用してください" }
+  validates :first_name_yomi, format: { with: /\A[ァ-ヶー－]+\z/, message: "には全角カタカナを使用してください" }
+
+end
