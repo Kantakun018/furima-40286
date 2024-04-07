34e0ae755e9a00f8410caeef40e501738dda7cbb
Kantakun018 <kan.xxi.18@gmail.com>
2024-04-08 08:25:38 +0900

    モデル単体テストコード記述

---
+++ spec/factories/users.rb
@@ -0,0 +1,16 @@
+FactoryBot.define do
+  factory :user do
+    transient do
+      person { Gimei.name }
+    end
+    nickname              {Faker::Name.initials(number: 2)}
+    email                 {Faker::Internet.email}
+    password              {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+    last_name             {person.last.kanji}
+    first_name            {person.first.kanji}
+    last_name_yomi        {person.last.katakana}
+    first_name_yomi       {person.first.katakana}
+    birthday              {Faker::Date.backward}
+  end
+end
