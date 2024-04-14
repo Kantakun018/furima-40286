2e5dd392cd7c8e35fc6a729015ec88a8c530d150
Kantakun018 <kan.xxi.18@gmail.com>
2024-04-14 21:34:53 +0900

    商品情報編集機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,7 +22,23 @@ class ItemsController < ApplicationController
 
   def show
     # 選択された対象のitemの情報を取得
-      @item = Item.find(params[:id])
+    @item = Item.find(params[:id])
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+    if current_user.id != @item.user_id
+      redirect_to root_path
+    end
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item.id)
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   private
